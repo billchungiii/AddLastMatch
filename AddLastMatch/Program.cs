0e2c39b6c5569cebe8dcd95d4350cd95f0bbaa01
Bill Chung <bill_chungiii@hotmail.com>
2020-03-12 13:35:18 +0800

    加入專案檔案。

---
+++ AddLastMatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AddLastMatch
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            foreach (var item in await ReadAsync("TextFile1.txt"))
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+            Console.ReadLine();
+        }
+
+        async static ValueTask<Dictionary<string, string>> ReadAsync(string path)
+        {
+            var result = new Dictionary<string, string>();
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (await reader.ReadLineAsync() is string line)
+                {
+                    var content = line.Split(',');
+                    result[content[1]] = content[0];
+                }
+            }
+            return result;
+        }
+    }
+}
+
